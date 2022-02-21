6b4f1ffdfdb734e86f25b499f72939b77ce749dd
sean-vdw <62733062+sean-vdw@users.noreply.github.com>
2022-02-21 12:48:44 -0800

    added middlewares

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,3 +1,5 @@
+const Users = require('../users/users-model');
+
 /*
   If the user does not have a session saved in the server
 
@@ -6,9 +8,13 @@
     "message": "You shall not pass!"
   }
 */
-function restricted() {
-
-}
+function restricted(req, res, next) {
+  if (req.session.user) {
+    next();
+  } else {
+    next({ status: 401, message: 'You shall not pass!' });
+  };
+};
 
 /*
   If the username in req.body already exists in the database
@@ -18,9 +24,19 @@ function restricted() {
     "message": "Username taken"
   }
 */
-function checkUsernameFree() {
-
-}
+async function checkUsernameFree(req, res, next) {
+  const { username } = req.body;
+  try {
+    const existing = await Users.findBy({ username });
+    if (existing) {
+      next({ status: 401, message: 'Username taken'});
+    } else {
+      next();
+    }
+  } catch(err) {
+    next(err);
+  };
+};
 
 /*
   If the username in req.body does NOT exist in the database
@@ -30,9 +46,19 @@ function checkUsernameFree() {
     "message": "Invalid credentials"
   }
 */
-function checkUsernameExists() {
-
-}
+async function checkUsernameExists(req, res, next) {
+  const { username } = req.body;
+  try {
+    const existing = Users.findBy({ username });
+    if (!existing) {
+      next({ status: 401, message: 'Invalid credentials' });
+    } else {
+      next();
+    }
+  } catch(err) {
+    next(err);
+  };
+};
 
 /*
   If password is missing from req.body, or if it's 3 chars or shorter
@@ -43,7 +69,19 @@ function checkUsernameExists() {
   }
 */
 function checkPasswordLength() {
-
-}
+  const { password } = req.body;
+  if (!password || password.length < 4) {
+    next({ status: 422, message: 'Password must be longer than 3 chars' });
+  } else {
+    next();
+  };
+};
 
 // Don't forget to add these to the `exports` object so they can be required in other modules
+
+module.exports = {
+  restricted, 
+  checkUsernameFree,
+  checkUsernameExists,
+  checkPasswordLength
+}
